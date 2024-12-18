e3cadf910a719b571c80e6c191f516fbf0931077
royalrock11 <royalrock13@gmail.com>
2024-12-18 09:03:12 -0500

    Makefile additions

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -18,14 +18,15 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     uint256 public AMOUNT_TO_SEND = AMOUNT_TO_CLAIM * 4;
     bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
-    bytes32[]  PROOF = [proofOne, proofTwo];
+    bytes32[] PROOF = [proofOne, proofTwo];
+    address public gasPayer;
     address user;
     uint256 userPrivateKey;
 
     function setUp() public {
         if (!isZkSyncChain()) {
-          DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
-          (airdrop, token) = deployer.deployMerkleAirdrop();
+            DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
+            (airdrop, token) = deployer.deployMerkleAirdrop();
         } else {
             token = new BagelToken();
             airdrop = new MerkleAirdrop(ROOT, token);
@@ -33,21 +34,23 @@ contract MerkleAirdropTest is ZkSyncChainChecker, Test {
             token.transfer(address(airdrop), AMOUNT_TO_SEND);
         }
         (user, userPrivateKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
     }
-
 
     function testUsersCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
-        
+        bytes32 digest = airdrop.getMessageHash(user, AMOUNT_TO_CLAIM);
 
         console.log("User starting balance: ", startingBalance);
 
         vm.startPrank(user);
-        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
         vm.stopPrank();
 
+        vm.prank(gasPayer);
+        airdrop.claim(user, AMOUNT_TO_CLAIM, PROOF, v, r, s);
+
         uint256 endingBalance = token.balanceOf(user);
-       
 
         console.log("User ending balance: ", endingBalance);
 
